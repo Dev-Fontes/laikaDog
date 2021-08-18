efa688d91e50d2d6603ba346447477bf5f0560c0
Icaro Fontes <icaro.fontes@lifedental.com.br>
2021-08-18 11:09:17 -0300

    Using axios, useEffect and state

--- src/modules/counter/components/styled.js
+++ src/modules/counter/components/styled.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
 export const CounterCustom = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-width: 100%;
-
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  flex-direction: column;
 `;
--- src/modules/counter/index.js
+++ src/modules/counter/index.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import * as S from "./components/styled";
 
 export const Counter = () => {
-  return <S.CounterCustom>Teste</S.CounterCustom>;
+  const [dados, setDados] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      setDados(res.data);
+    });
+  }, []);
+
+  return (
+    <S.CounterCustom>
+      {dados.map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </S.CounterCustom>
+  );
 };
